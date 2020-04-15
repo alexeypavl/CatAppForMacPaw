2c2030a62c58956ef08b4a63f1042dc7cd73e160
Alexey Pavluk <ialexpavl@icloud.com>
2020-04-15 18:34:32 +0300

    First Commit

---
+++ CatAppForMacPaw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatAppForMacPaw
+//
+//  Created by MacBook Pro on 15.04.2020.
+//  Copyright © 2020 CocoaOneLove. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
